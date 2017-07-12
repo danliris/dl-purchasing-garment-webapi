8cc3098a70f88c637bf3909e96e87e957f621c62
indriHutabalian <indri.hutabalian@moonlay.com>
2017-07-12 16:06:30 +0700

    add purchasing garment api

---
+++ routes/v1.js
@@ -0,0 +1,13 @@
+// PURCHASE REQUEST
+var v1PurchaseRequestByUserRouter = require('../src/routers/v1/purchase-request/purchase-request-by-user-router');
+var v1PurchaseRequestMonitoringRouter = require('../src/routers/v1/purchase-request/purchase-request-monitoring-router');
+var v1PurchaseRequestMonitoringAllUserRouter = require('../src/routers/v1/purchase-request/purchase-request-monitoring-all-user-router');
+var v1PurchaseRequestRouter = require('../src/routers/v1/purchase-request/purchase-request-router');
+
+module.exports = function (server) {
+    //PURCHASE REQUEST
+    v1PurchaseRequestByUserRouter().applyRoutes(server, "/v1/purchase-requests/by-user");
+    v1PurchaseRequestMonitoringRouter().applyRoutes(server, "/v1/purchase-requests/monitoring");
+    v1PurchaseRequestMonitoringAllUserRouter().applyRoutes(server, "/v1/purchase-requests/monitoring-all-user");
+    v1PurchaseRequestRouter().applyRoutes(server, "/v1/purchase-requests");
+};
