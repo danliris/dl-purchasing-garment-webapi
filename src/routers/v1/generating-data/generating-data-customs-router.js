730c6a81780fe1554c51eb4d5235a08594b64702
AndreaZain <andrea.kinan@gmail.com>
2018-01-27 08:59:57 +0700

    Modify API
    Modify Router API - Purchasing Garment

--- src/routers/v1/generating-data/generating-data-customs-router.js
+++ src/routers/v1/generating-data/generating-data-customs-router.js
@@ -11,139 +11,84 @@ function getRouter() {
     router.get('/', passport, (request, response, next) => {
         db.get().then(db => {
             var manager = new CustomsManager(db, request.user);
-
-            var dateFormat = "MM/DD/YYYY";
-            var locale = 'en-US';
-            var moment = require('moment');
-            moment.locale(locale);
-            var dateFrom = request.params.dateFrom;
-            var dateTo = request.params.dateTo;
-            var filter = {};
-            if (dateFrom && dateTo) {
-                filter = {
-                    customsDate: {
-                        $gte: new Date(dateFrom),
-                        $lte: new Date(dateTo)
+            var sdate = request.params.dateFrom;
+            var edate = request.params.dateTo;
+            var offset = 7//request.headers["x-timezone-offset"] ? Number(request.headers["x-timezone-offset"]) : 0;
+            
+            manager.getAllData(sdate, edate, offset)
+                .then(docs => {
+                if ((request.headers.accept || '').toString().indexOf("application/xls") < 0) {
+                        var result = resultFormatter.ok(apiVersion, 200, docs);
+                        response.send(200, result);
                     }
-                };
-            }
-
-            manager.getAllData(filter)
-                .then(list => {
-                    var Tqty = 0
-                    var Thrg = 0
+                else {
+                    var dateFormat = "MM/DD/YYYY";
+                    var locale = 'en-US';
+                    var moment = require('moment');
+                    moment.locale(locale);
                     var data = [];
-                    for (var _data of list) {
-                        for (var SJ of _data.deliveryOrders) {
-                            for (var item of SJ.items) {
-                                for (var fulfillment of item.fulfillments) {
-                                     var _item = {
-                                     "NOMOR BEA CUKAI": _data.no,
-                                     "TANGGAL BEA CUKAI": moment(new Date(_data.customsDate)).format(dateFormat),
-                                     "TANGGAL PENGESAHAN": moment(new Date(_data.validateDate)).format(dateFormat),
-                                     "TIPE BEA CUKAI": _data.customsType,
-                                     "ASAL BEA CUKAI": _data.customsOrigin,
-                                     "KODE SUPPLIER": _data.supplier.code,
-                                     "NAMA SUPPLIER": _data.supplier.name,
-                                     "JUMLAH KEMASAN": _data.amountOfPackaging,
-                                     "KEMASAN": _data.packaging,
-                                     "BRUTTO (KG)": _data.bruto,
-                                     "NETTO (KG)": _data.netto,
-                                     "MATA UANG": _data.currency.code,
-                                     "NOMOR SURAT JALAN": SJ.no,
-                                     "TANGGAL SURAT JALAN": moment(new Date(SJ.date)).format(dateFormat),
-                                     "TANGGAL BARANG DATANG": moment(new Date(SJ.supplierDoDate)).format(dateFormat),
-                                     "NOMOR PO EXTERNAL": item.purchaseOrderExternalNo,
-                                     "NOMOR PURCHASE REQUEST": fulfillment.purchaseRequestNo,
-                                     "KODE BARANG": fulfillment.product.code,
-                                     "NAMA BARANG": fulfillment.product.name +" - " + fulfillment.product.description,
-                                     "JUMLAH BARANG": fulfillment.deliveredQuantity,
-                                     "SATUAN BARANG": fulfillment.purchaseOrderUom.unit,
-                                     "HARGA SATUAN BARANG": fulfillment.pricePerDealUnit,
-                                     "HARGA TOTAL BARANG": fulfillment.pricePerDealUnit * fulfillment.deliveredQuantity,
-                                     "USER INPUT": _data._createdBy,
-                                     "TANGGAL INPUT": moment(new Date(_data._createdDate)).format(dateFormat),
-                                     "USER EDIT": _data._updatedBy,
-                                     "TANGGAL EDIT": moment(new Date(_data._updatedDate)).format(dateFormat)
-                                     }
-                                     data.push(_item);
+                    for (var _data of docs) {
+                   
+                           var NoBC= _data._id.NoBC;
+                           var TgBC= moment(new Date(_data._id.TgBC)).add(offset, 'h').format(dateFormat);
+                           var TgValid= moment(new Date(_data._id.TgValid)).add(offset, 'h').format(dateFormat);
+                           var TipeBC= _data._id.TipeBC;
+                           var QtyBC = _data._id.QtyBC;
+                           var SatBC = _data._id.SatBC;
+                           var Brutto=_data._id.Brutto;
+                           var Netto=_data._id.Netto;
+                           var TgIn= moment(new Date(_data._id.TgIn)).add(offset, 'h').format(dateFormat);
+                           var UserIn= _data._id.UserIn;
+                           var TgEd= moment(new Date(_data._id.TgEd)).add(offset, 'h').format(dateFormat);
+                           var UserEd = _data._id.UserEd;
+                           
+                           var _item = {
+                                "NoBC" : _data._id.NoBC,
+                                "TgBC" : moment(new Date(_data._id.TgBC)).add(offset, 'h').format(dateFormat),
+                                "TgValid" : moment(new Date(_data._id.TgBC)).add(offset, 'h').format(dateFormat),
+                                "TipeBC" : _data._id.TipeBC,
+                                "QtyBC" : _data._id.QtyBC,
+                                "SatBC" : _data._id.SatBC,
+                                "Brutto" : _data._id.Brutto,
+                                "Netto":_data._id.Netto,
+                                "TgIn" : moment(new Date(_data._id.TgIn)).add(offset, 'h').format(dateFormat),
+                                "UserIn" : _data._id.UserIn,
+                                "TgEd" : moment(new Date(_data._id.TgEd)).add(offset, 'h').format(dateFormat),
+                                "UserEd" : _data._id.UserEd                                
                             }
-                          }
-                        }
-                    }
-                    var options = {
-                        "NOMOR BEA CUKAI": "string",
-                        "TANGGAL BEA CUKAI": "date",
-                        "TANGGAL PENGESAHAN": "date",
-                        "TIPE BEA CUKAI": "string",
-                        "ASAL BEA CUKAI": "string",
-                        "KODE SUPPLIER": "string",
-                        "NAMA SUPPLIER": "string",
-                        "JUMLAH KEMASAN": "number",
-                        "KEMASAN": "string",
-                        "BRUTO (KG)": "number",
-                        "NETTO (KG)": "number",
-                        "KEMASAN": "string",
-                        "NOMOR SURAT JALAN": "string",
-                        "TANGGAL SURAT JALAN": "date",
-                        "TANGGAL BARANG DATANG": "date",
-                        "NOMOR PO EXTERNAL": "string",
-                        "NOMOR PURCHASE REQUEST": "string",
-                        "KODE BARANG": "string",
-                        "NAMA BARANG": "string",
-                        "JUMLAH BARANG": "number",
-                        "SATUAN BARANG": "string",
-                        "HARGA SATUAN BARANG": "number",
-                        "HARGA TOTAL BARANG": "number",
-                        "USER INPUT": "string",
-                        "TANGGAL INPUT": "date",
-                        "USER EDIT": "string",
-                        "TANGGAL EDIT": "date"
-                    };
+                                data.push(_item);
+                        }    
 
-                    if (data.length === 0) {
-                        var _item = {
-                        "NOMOR BEA CUKAI": "",
-                        "TANGGAL BEA CUKAI": "",
-                        "TANGGAL PENGESAHAN": "",
-                        "TIPE BEA CUKAI": "",
-                        "ASAL BEA CUKAI": "",
-                        "KODE SUPPLIER": "",
-                        "NAMA SUPPLIER": "",
-                        "JUMLAH KEMASAN": "",
-                        "KEMASAN": "",
-                        "BRUTO (KG)": "",
-                        "NETTO (KG)": "",
-                        "KEMASAN": "",
-                        "NOMOR SURAT JALAN": "",
-                        "TANGGAL SURAT JALAN": "",
-                        "TANGGAL BARANG DATANG": "",
-                        "NOMOR PO EXTERNAL": "",
-                        "NOMOR PURCHASE REQUEST": "",
-                        "KODE BARANG": "",
-                        "NAMA BARANG": "",
-                        "JUMLAH BARANG": "",
-                        "SATUAN BARANG": "",
-                        "HARGA SATUAN BARANG": "",
-                        "HARGA TOTAL BARANG": "",
-                        "USER INPUT": "",
-                        "TANGGAL INPUT": "",
-                        "USER EDIT": "",
-                        "TANGGAL EDIT": ""
-                        }
-                        data.push(_item);
-                    }
-                    if (dateFrom && dateTo) {
-                        response.xls(`Garment Bea Cukai.xlsx`, data, options);
-                    } else {
-                        response.xls(`Garment Bea Cukai.xlsx`, data, options);
-                    }
+                        var options = {
+                                        "NoBC" : "string",
+                                        "TgBC" : "date",
+                                        "TgValid" : "date",
+                                        "TipeBC" : "string",
+                                        "QtyBC" : "number",
+                                        "SatBC" : "string",
+                                        "Brutto" : "number",
+                                        "Netto": "number",
+                                        "TgIn" : "date",
+                                        "UserIn" : "string",
+                                        "TgEd" : "date",
+                                        "UserEd" : "string"                                             
+                                       };
+                   
+                     if(sdate!=undefined && edate!=undefined)
+                      {
+                        response.xls(`Garment Bea Cukai.xlsx`, data,options);
+                      } else 
+                        response.xls(`Garment Bea Cukai.xlsx`, data,options);
+                      }
                 })
                 .catch(e => {
+                    response.send(500, "Collecting Data Failed...!!!");
+                });
+
+              }).catch(e => {
                     var error = resultFormatter.fail(apiVersion, 400, e);
                     response.send(400, error);
-                })
-        })
+            });
     });
     return router;
 }
