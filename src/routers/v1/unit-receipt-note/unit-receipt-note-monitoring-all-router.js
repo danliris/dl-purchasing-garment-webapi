67a52d88d627a8df2f8ac96bce6936389eec5de4
kristika <kristika.sachi@gmail.com>
2018-02-23 12:19:20 +0700

    Update router
    tambah router monitoring bon all

---
+++ src/routers/v1/unit-receipt-note/unit-receipt-note-monitoring-all-router.js
@@ -0,0 +1,55 @@
+var Router = require('restify-router').Router;
+var db = require("../../../db");
+var UnitReceiptManager = require("dl-module").managers.garmentPurchasing.UnitReceiptNoteManager;
+var resultFormatter = require("../../../result-formatter");
+
+var passport = require('../../../passports/jwt-passport');
+const apiVersion = '1.0.0';
+
+function getRouter() {
+
+    var getManager = (user) => {
+        return db.get()
+            .then((db) => {
+                return Promise.resolve(new UnitReceiptManager(db, user));
+            });
+    };
+
+    var router = new Router();
+    router.get("/", passport, function (request, response, next) {
+        var user = request.user;
+        var query = request.query;
+     
+        var UnitReceiptManager = {};
+        getManager(user)
+            .then((manager) => {
+                UnitReceiptManager = manager;
+                return UnitReceiptManager.getUnitReceiptAllReport(query);
+            })
+            .then(docs => {
+                var result = resultFormatter.ok(apiVersion, 200, docs);
+                return Promise.resolve(result);
+            })
+            .then((result) => {
+                if ((request.headers.accept || '').toString().indexOf("application/xls") < 0) {
+                    response.send(result.statusCode, result);
+                }
+                else{
+                    UnitReceiptManager.getUnitReceiptAllReportXls(result, query)
+                        .then(xls => {
+                            response.xls(xls.name, xls.data, xls.options)
+                        });
+                }
+            })
+            .catch((e) => {
+                var statusCode = 500;
+                if (e.name === "ValidationError")
+                    statusCode = 400;
+                var error = resultFormatter.fail(apiVersion, statusCode, e);
+                response.send(statusCode, error);
+            });
+    });
+    return router;
+}
+
+module.exports = getRouter;
